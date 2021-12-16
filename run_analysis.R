c1aff293596d6de6385c571d19db346cca406359
aerohiver <74781242+aerohiver@users.noreply.github.com>
2021-12-16 22:48:19 +0900

    Update run_analysis.R
    upadate_again

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,13 @@
+# This script performs the following steps for the wearable dataset collected from the link below.
+# This is for the final course project for the course Getting and Cleaning Data on Coursera
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  
+
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 # ====== Part 1 ======= 
 # Reading the test files and Checking Heads
 test_subjects_index<- read.table("./UCI HAR Dataset/test/subject_test.txt", sep = " ")
